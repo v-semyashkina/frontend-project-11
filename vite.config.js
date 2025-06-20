0d89586b03f82d58c3dc11106759dc61f02e57ed
Semyashkina <forty.two2010@gmail.com>
2025-06-20 17:17:59 +0300

    linter

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
   root: path.resolve(__dirname, ''),
