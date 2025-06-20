f64da239d4c1fc7baf7c1c1a29813e0823d00b0c
Semyashkina <forty.two2010@gmail.com>
2025-06-20 17:00:31 +0300

    linter fix

--- src/init.js
+++ src/init.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import { Modal } from 'bootstrap';
-import { object, string } from 'yup';
-import axios from 'axios';
-import { uniqueId } from 'lodash';
-import i18n from 'i18next';
-import ru from './locales/ru.js';
-import watchState from './view.js';
-import parse from './parse.js';
-import refresh from './refresh.js';
+import { Modal } from 'bootstrap'
+import { object, string } from 'yup'
+import axios from 'axios'
+import { uniqueId } from 'lodash'
+import i18n from 'i18next'
+import ru from './locales/ru.js'
+import watchState from './view.js'
+import parse from './parse.js'
+import refresh from './refresh.js'
 
 export default () => {
   i18n
@@ -36,88 +36,90 @@ export default () => {
           link: '',
         },
         seenPosts: new Set([]),
-      };
+      }
 
-      const getLink = (link) => `https://allorigins.hexlet.app/get?disableCache=true&url=${link}`;
+      const getLink = link => `https://allorigins.hexlet.app/get?disableCache=true&url=${link}`
 
       const schema = object().shape({
         newUrl: string('urlInvalid')
           .required('urlInvalid')
           .url('urlInvalid')
           .test('not-in-feeds', 'rssExists', function (value) {
-            const { existingFeeds } = this.parent;
-            return !existingFeeds.includes(value);
+            const { existingFeeds } = this.parent
+            return !existingFeeds.includes(value)
           }),
-      });
+      })
 
-      const form = document.querySelector('.rss-form');
-      const input = document.querySelector('#url-input');
+      const form = document.querySelector('.rss-form')
+      const input = document.querySelector('#url-input')
 
       form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const newUrl = input.value.trim();
-        const existingFeeds = state.feeds.map((feed) => feed.feedLink);
-        const dataToValidate = { newUrl, existingFeeds };
+        e.preventDefault()
+        const newUrl = input.value.trim()
+        const existingFeeds = state.feeds.map(feed => feed.feedLink)
+        const dataToValidate = { newUrl, existingFeeds }
         schema
           .validate(dataToValidate, { abortEarly: false })
           .then(() => {
-            state.form = { ...state.form, error: 'noError', isValid: true };
+            state.form = { ...state.form, error: 'noError', isValid: true }
           })
           .then(() => {
-            state.loadingProcess = { ...state.loadingProcess, state: 'loading' };
-            const proxyLink = getLink(newUrl);
-            return axios.get(proxyLink);
+            state.loadingProcess = { ...state.loadingProcess, state: 'loading' }
+            const proxyLink = getLink(newUrl)
+            return axios.get(proxyLink)
           })
           .then((response) => {
-            const responseStatus = response.status;
+            const responseStatus = response.status
             if (responseStatus !== 200) {
               state.loadingProcess = {
                 ...state.loadingProcess,
                 state: 'failed',
                 error: 'rssInvalid',
-              };
-            } else {
-              const data = response.data.contents;
-              const parsedData = parse(data);
-              const parseError = parsedData.getElementsByTagName('parsererror');
+              }
+            }
+            else {
+              const data = response.data.contents
+              const parsedData = parse(data)
+              const parseError = parsedData.getElementsByTagName('parsererror')
               if (parseError.length > 0) {
                 state.loadingProcess = {
                   ...state.loadingProcess,
                   state: 'failed',
                   error: 'rssInvalid',
-                };
-              } else {
-                const feedTitle = parsedData.getElementsByTagName('title')[0].textContent;
-                const feedDescription =
-                  parsedData.getElementsByTagName('description')[0].textContent;
-                const feedLink = newUrl;
-                const id = uniqueId();
-                state.feeds = [...state.feeds, { id, feedTitle, feedDescription, feedLink }];
-                const posts = Array.from(parsedData.getElementsByTagName('item'));
+                }
+              }
+              else {
+                const feedTitle = parsedData.getElementsByTagName('title')[0].textContent
+                const feedDescription
+                  = parsedData.getElementsByTagName('description')[0].textContent
+                const feedLink = newUrl
+                const id = uniqueId()
+                state.feeds = [...state.feeds, { id, feedTitle, feedDescription, feedLink }]
+                const posts = Array.from(parsedData.getElementsByTagName('item'))
                 const newPosts = posts.map((post) => {
-                  const postGuid = post.getElementsByTagName('guid')[0].textContent;
-                  const postTitle = post.getElementsByTagName('title')[0].textContent;
-                  const postDescription = post.getElementsByTagName('description')[0].textContent;
-                  const postLink = post.getElementsByTagName('link')[0].textContent;
-                  const postId = uniqueId();
-                  return { id: postId, feedId: id, postTitle, postDescription, postLink, postGuid };
-                });
-                state.posts = [...newPosts, ...state.posts];
+                  const postGuid = post.getElementsByTagName('guid')[0].textContent
+                  const postTitle = post.getElementsByTagName('title')[0].textContent
+                  const postDescription = post.getElementsByTagName('description')[0].textContent
+                  const postLink = post.getElementsByTagName('link')[0].textContent
+                  const postId = uniqueId()
+                  return { id: postId, feedId: id, postTitle, postDescription, postLink, postGuid }
+                })
+                state.posts = [...newPosts, ...state.posts]
                 state.loadingProcess = {
                   ...state.loadingProcess,
                   state: 'success',
                   error: 'noError',
-                };
+                }
               }
             }
           })
           .then(() => {
             const feeds = state.feeds.map((feed) => {
-              const id = feed.id;
-              const link = feed.feedLink;
-              return { id, link };
-            });
-            refresh(feeds);
+              const id = feed.id
+              const link = feed.feedLink
+              return { id, link }
+            })
+            refresh(feeds)
           })
           .catch((e) => {
             if (e.name === 'AxiosError') {
@@ -125,49 +127,49 @@ export default () => {
                 ...state.loadingProcess,
                 state: 'failed',
                 error: 'networkError',
-              };
-            } else {
-              console.log(e);
-              const message = e.errors[0];
-              state.form = { ...state.form, error: message, isValid: false };
+              }
             }
-          });
-      });
+            else {
+              console.log(e)
+              const message = e.errors[0]
+              state.form = { ...state.form, error: message, isValid: false }
+            }
+          })
+      })
 
       const updateModal = (post) => {
-        console.log('modal updated');
         state.modal = {
           ...state.modal,
           title: post.postTitle,
           description: post.postDescription,
           link: post.postLink,
-        };
-      };
+        }
+      }
 
       const addPostToSeen = (id) => {
-        state.seenPosts.add(id);
-      };
+        state.seenPosts.add(id)
+      }
 
-      const state = watchState(initialState, i18n, updateModal, addPostToSeen);
+      const state = watchState(initialState, i18n, updateModal, addPostToSeen)
 
       const update = () => {
         return refresh(state, getLink)
           .then((newPosts) => {
             if (newPosts.length > 0) {
-              state.posts = [...newPosts, ...state.posts];
+              state.posts = [...newPosts, ...state.posts]
             }
           })
           .catch((e) => {
-            console.log(e);
+            console.log(e)
           })
           .finally(() => {
-            setTimeout(update, 5000);
-          });
-      };
+            setTimeout(update, 5000)
+          })
+      }
 
-      update();
+      update()
     })
     .catch((e) => {
-      console.log(e);
-    });
-};
+      console.log(e)
+    })
+}
--- src/render.js
+++ src/render.js
@@ -1,52 +1,51 @@
 const renderFeeds = (feeds, feedsHeader) => {
-  const feedsContainer = document.querySelector('.feeds');
-  feedsContainer.textContent = '';
-  const card = document.createElement('div');
-  card.classList.add('card', 'border-0');
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
-  const header = document.createElement('h2');
-  header.classList.add('card-title', 'h4');
-  header.textContent = feedsHeader;
-  const list = document.createElement('ul');
-  list.classList.add('list-group', 'border-0', 'rounded-0');
-  cardBody.append(header);
-  card.append(cardBody);
+  const feedsContainer = document.querySelector('.feeds')
+  feedsContainer.textContent = ''
+  const card = document.createElement('div')
+  card.classList.add('card', 'border-0')
+  const cardBody = document.createElement('div')
+  cardBody.classList.add('card-body')
+  const header = document.createElement('h2')
+  header.classList.add('card-title', 'h4')
+  header.textContent = feedsHeader
+  const list = document.createElement('ul')
+  list.classList.add('list-group', 'border-0', 'rounded-0')
+  cardBody.append(header)
+  card.append(cardBody)
   feeds.forEach((feed) => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-group-item', 'border-0', 'border-end-0');
-    const title = document.createElement('h3');
-    title.classList.add('h6', 'm-0');
-    title.textContent = feed.feedTitle;
-    const description = document.createElement('p');
-    description.classList.add('m-0', 'small', 'text-black-50');
-    description.textContent = feed.feedDescription;
-    listItem.append(title);
-    listItem.append(description);
-    list.prepend(listItem);
-  });
-  feedsContainer.append(card);
-  feedsContainer.append(list);
-};
+    const listItem = document.createElement('li')
+    listItem.classList.add('list-group-item', 'border-0', 'border-end-0')
+    const title = document.createElement('h3')
+    title.classList.add('h6', 'm-0')
+    title.textContent = feed.feedTitle
+    const description = document.createElement('p')
+    description.classList.add('m-0', 'small', 'text-black-50')
+    description.textContent = feed.feedDescription
+    listItem.append(title)
+    listItem.append(description)
+    list.prepend(listItem)
+  })
+  feedsContainer.append(card)
+  feedsContainer.append(list)
+}
 
 const renderPosts = (state, postsHeader, buttonText, updateModal, addPostToSeen) => {
-  const { posts, seenPosts } = state;
-  const postContainer = document.querySelector('.posts');
-  postContainer.textContent = '';
-  const card = document.createElement('div');
-  card.classList.add('card', 'border-0');
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
-  const header = document.createElement('h2');
-  header.classList.add('card-title', 'h4');
-  header.textContent = postsHeader;
-  const list = document.createElement('ul');
-  list.classList.add('list-group', 'border-0', 'rounded-0');
-  cardBody.append(header);
-  card.append(cardBody);
+  const { posts, seenPosts } = state
+  const postContainer = document.querySelector('.posts')
+  postContainer.textContent = ''
+  const card = document.createElement('div')
+  card.classList.add('card', 'border-0')
+  const cardBody = document.createElement('div')
+  cardBody.classList.add('card-body')
+  const header = document.createElement('h2')
+  header.classList.add('card-title', 'h4')
+  header.textContent = postsHeader
+  const list = document.createElement('ul')
+  list.classList.add('list-group', 'border-0', 'rounded-0')
+  cardBody.append(header)
+  card.append(cardBody)
   posts.forEach((post) => {
-    console.log(post);
-    const listItem = document.createElement('li');
+    const listItem = document.createElement('li')
     listItem.classList.add(
       'list-group-item',
       'd-flex',
@@ -54,33 +53,33 @@ const renderPosts = (state, postsHeader, buttonText, updateModal, addPostToSeen)
       'align-items-start',
       'border-0',
       'border-end-0',
-    );
-    const link = document.createElement('a');
+    )
+    const link = document.createElement('a')
     seenPosts.has(post.id)
       ? link.classList.add('fw-normal', 'link-secondary')
-      : link.classList.add('fw-bold');
-    link.href = post.postLink;
-    link.textContent = post.postTitle;
-    link.target = '_blank';
-    link.rel = 'noopener noreferrer';
-    link.dataset.id = post.id;
-    listItem.append(link);
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-    button.dataset.id = post.id;
-    button.dataset.bsToggle = 'modal';
-    button.dataset.bsTarget = '#modal';
-    button.textContent = buttonText;
+      : link.classList.add('fw-bold')
+    link.href = post.postLink
+    link.textContent = post.postTitle
+    link.target = '_blank'
+    link.rel = 'noopener noreferrer'
+    link.dataset.id = post.id
+    listItem.append(link)
+    const button = document.createElement('button')
+    button.type = 'button'
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm')
+    button.dataset.id = post.id
+    button.dataset.bsToggle = 'modal'
+    button.dataset.bsTarget = '#modal'
+    button.textContent = buttonText
     button.addEventListener('click', () => {
-      updateModal(post);
-      addPostToSeen(post.id);
-    });
-    listItem.append(button);
-    list.append(listItem);
-  });
-  postContainer.append(card);
-  postContainer.append(list);
-};
+      updateModal(post)
+      addPostToSeen(post.id)
+    })
+    listItem.append(button)
+    list.append(listItem)
+  })
+  postContainer.append(card)
+  postContainer.append(list)
+}
 
-export { renderFeeds, renderPosts };
+export { renderFeeds, renderPosts }
