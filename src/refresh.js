53cbe5396439daf167bfe9e650c0027827828b8c
Semyashkina <forty.two2010@gmail.com>
2025-06-23 15:55:44 +0300

    lint&qlty

--- src/refresh.js
+++ src/refresh.js
@@ -1,52 +1,36 @@
-import axios from 'axios';
-import parse from './parse.js';
-import { uniqueId } from 'lodash';
+import axios from 'axios'
+import parse from './parse.js'
 
 export default (state, getLink, processPosts) => {
-  const { feeds, posts } = state;
+  const { feeds, posts } = state
   return Promise.all(
     (feeds || []).map((feed) => {
-      const proxyLink = getLink(feed.feedLink);
+      const proxyLink = getLink(feed.feedLink)
       const oldPostsGuids = posts
-        .filter((post) => post.feedId === feed.id)
-        .map((post) => post.postGuid);
+        .filter(post => post.feedId === feed.id)
+        .map(post => post.postGuid)
       return axios
         .get(proxyLink)
         .then((response) => {
-          const data = response.data.contents;
-          const parsedData = parse(data);
-          const posts = Array.from(parsedData.getElementsByTagName('item'));
+          const data = response.data.contents
+          const parsedData = parse(data)
+          const posts = Array.from(parsedData.getElementsByTagName('item'))
           const newPosts = posts.filter((post) => {
-            const postGuid = post.getElementsByTagName('guid')[0].textContent;
-            return !oldPostsGuids.includes(postGuid);
-          });
+            const postGuid = post.getElementsByTagName('guid')[0].textContent
+            return !oldPostsGuids.includes(postGuid)
+          })
           if (newPosts.length > 0) {
-            const processedPosts = processPosts(newPosts, feed.id);
-            return processedPosts;
-            // return newPosts.map((post) => {
-            //   const postGuid = post.getElementsByTagName('guid')[0].textContent;
-            //   const postTitle = post.getElementsByTagName('title')[0].textContent;
-            //   const postDescription = post.getElementsByTagName('description')[0].textContent;
-            //   const postLink = post.getElementsByTagName('link')[0].textContent;
-            //   const postId = uniqueId();
-            //   return {
-            //     id: postId,
-            //     feedId: feed.id,
-            //     postTitle,
-            //     postDescription,
-            //     postLink,
-            //     postGuid,
-            //   };
-            // });
+            const processedPosts = processPosts(newPosts, feed.id)
+            return processedPosts
           }
-          return [];
+          return []
         })
         .catch((e) => {
-          console.log(e);
-          return [];
-        });
+          console.log(e)
+          return []
+        })
     }),
   ).then((results) => {
-    return results.flat();
-  });
-};
+    return results.flat()
+  })
+}
